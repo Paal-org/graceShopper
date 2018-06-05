c25b771e01eab68ab3ea296a98addbc4fc60374e
angel-chen <rikugou@gmail.com>
2018-06-05 16:29:21 -0400

    Fix errors with api route for all products

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product } = require('../db');
+const { Product } = require('../db/models');
 
 router.get('/', async (req, res, next) => {
   try {
@@ -12,4 +12,4 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-export default router
+module.exports = router
