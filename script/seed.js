adfcc7402a6401f4bc29542bafeac619fda29d63
PanktiParikh <panktip85@gmail.com>
2018-06-05 13:02:53 -0400

    seed file updated

--- script/seed.js
+++ script/seed.js
@@ -21,8 +21,18 @@ async function seed() {
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
   const users = await Promise.all([
-    User.create({ email: 'cody@email.com', password: '123' }),
-    User.create({ email: 'murphy@email.com', password: '123' }),
+    User.create({
+      firstName: 'Cody',
+      lastName: 'puppy',
+      email: 'cody@email.com',
+      password: '123',
+    }),
+    User.create({
+      firstName: 'Murphy',
+      lastName: 'puppy',
+      email: 'murphy@email.com',
+      password: '123',
+    }),
   ]);
 
   //************* DRINKS *******************/
@@ -128,7 +138,7 @@ async function seed() {
       category: 'food',
     }),
   ]);
-
+  db.close();
   // Wowzers! We can even `await` on the right-hand side of the assignment operator
   // and store the result that the promise resolves to in a variable! This is nice!
   console.log(`seeded ${users.length} users`);
@@ -141,17 +151,16 @@ async function seed() {
 // `Async` functions always return a promise, so we can use `catch` to handle
 // any errors that might occur inside of `seed`.
 if (module === require.main) {
-  seed()
-    .catch(err => {
-      console.error(err);
-      process.exitCode = 1;
-    })
-    .finally(() => {
-      // `finally` is like then + catch. It runs no matter what.
-      console.log('closing db connection');
-      db.close();
-      console.log('db connection closed');
-    });
+  seed().catch(err => {
+    console.error(err);
+    process.exitCode = 1;
+  });
+  // .finally(() => {
+  //   // `finally` is like then + catch. It runs no matter what.
+  //   console.log('closing db connection');
+  //   db.close();
+  //   console.log('db connection closed');
+  // });
   /*
    * note: everything outside of the async function is totally synchronous
    * The console.log below will occur before any of the logs that occur inside
