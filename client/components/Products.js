4049602f04320cf508b994534de86d16e7037c0b
PanktiParikh <panktip85@gmail.com>
2018-06-11 12:00:43 -0400

    pagination started

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,18 +1,40 @@
-import React from "react";
-import { connect } from "react-redux";
-import SingleProduct from "./SingleProduct";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SingleProduct from './SingleProduct';
+import Pagination from 'react-js-pagination';
 
-const Products = props => {
-  const { products } = props;
-  return (
-    <div className="card-columns">
-      {products.length
-        ? products.map(product => (
-            <SingleProduct product={product} key={product.id} />
-          ))
-        : "There are no products"}
-    </div>
-  );
-};
+class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+  handlePageChange(pageNumber) {
+    console.log(`active page is ${pageNumber}`);
+    this.setState({ activePage: pageNumber });
+  }
+  render() {
+    const { products } = this.props;
+    return (
+      <div>
+        <Pagination
+          hideDisabled
+          activePage={this.state.activePage}
+          itemsCountPerPage={4}
+          totalItemsCount={products.length}
+          onChange={this.handlePageChange}
+        />
+        <div className="card-columns">
+          {products.length
+            ? products.map(product => (
+                <SingleProduct product={product} key={product.id} />
+              ))
+            : 'There are no products'}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Products;
